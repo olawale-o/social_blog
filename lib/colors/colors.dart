3798f588d49f10539cf1ea44d8984a41866d2c7c
olawale-o <omoogunolawale@gmail.com>
2023-02-25 17:27:23 +0100

    Create colors

---
+++ lib/colors/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color blueColor = const Color(0xFF2663FF);
+Color darkColor = const Color(0xFF000000);
+Color darkGreyColor = const Color(0xFF616161);
+Color lightGreyColor = const Color(0xFFC6C6C6);
+Color whiteColor = const Color(0xFFFAFAFA);
+Color lemonColor = const Color(0xFF00D72F);
